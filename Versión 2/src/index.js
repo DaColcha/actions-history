b8e272b8a0f099b4f0c32ad257a83f1d9b75adcf
DarÃío Charro <dario14julian@gmail.com>
2024-03-06 11:39:17 -0500

    Calculo porcentaje

--- Versión 2/src/index.js
+++ Versión 2/src/index.js
@@ -1,3 +1,6 @@
+import { getActionCurrentPrice } from './api/service';
+import { createAction } from './database';
+import { getAll } from './database';
 
 //selectores
 const actionName = document.querySelector(".name");
@@ -5,6 +8,7 @@ const actionPrice = document.querySelector(".price");
 const actionQuantity = document.querySelector(".quantity");
 const actionButton = document.querySelector(".action-button");
 const actionList = document.querySelector(".action-list");
+
 
 //event listeners
 document.addEventListener("DOMContentLoaded", getactions);
@@ -24,12 +28,13 @@ function obtenerFechaEnFormato() {
 }
 
 //functions
-function addaction(event) {
+async function addaction(event) {
   //Prevent form submited
   event.preventDefault();
   //action div
   const actionDiv = document.createElement("div");
   actionDiv.classList.add("action");
+
 
   //create action object
   const action = {
@@ -38,6 +43,8 @@ function addaction(event) {
     price: actionPrice.value,
     quantity: actionQuantity.value,
     total: actionPrice.value * actionQuantity.value,
+    currentPrice: await getActionCurrentPrice(actionName.value),
+    change: ((currentPrice*100)/total)-100 + "%"
   };
 
   //Create LI
@@ -66,14 +73,27 @@ function addaction(event) {
   newTotal.classList.add("action-item");
   actionDiv.appendChild(newTotal);
 
+  const newCurrentPrice = document.createElement("li");
+  newCurrentPrice.innerText = action.currentPrice;
+  newCurrentPrice.classList.add("action-item");
+  actionDiv.appendChild(newCurrentPrice);
+
+  const newChange = document.createElement("li");
+  newChange.innerText = action.change;
+  newChange.classList.add("action-item");
+  actionDiv.appendChild(newChange);
+
+  console.log(action);
+
   //Add action to local
   saveLocalactions(action);
-
+  /*
   //Check trash button
   const trashButton = document.createElement("button");
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
   trashButton.classList.add("trash-btn");
-  actionDiv.appendChild(trashButton);
+  actionDiv.appendChild(trashButton);*/
+
   //Append to list
   actionList.appendChild(actionDiv);
   //Clear input
@@ -101,6 +121,10 @@ function deleteCheck(e) {
   }
 }
 
+function saveActions(){
+
+}
+
 function saveLocalactions(action) {
   //Check
   let actions;
@@ -114,8 +138,10 @@ function saveLocalactions(action) {
   localStorage.setItem("actions", JSON.stringify(actions));
 }
 
-function getactions() {
-  let actions;
+async function getactions() {
+  //let actions = await getAll();
+
+  
   if (localStorage.getItem("actions") === null) {
     actions = [];
   } else {
@@ -152,6 +178,7 @@ function getactions() {
     newTotal.classList.add("action-item");
     actionDiv.appendChild(newTotal);
 
+    
     //Check trash button
     const trashButton = document.createElement("button");
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
